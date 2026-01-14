ca065b95d32c7e29e61f73b9226c57eb11de63be
Artem <muha13artem@gmail.com>
2026-01-14 14:28:00 +0200

    start

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.getElementById('burger');
+
+  burger.addEventListener('click', () => {
+    if (burger.classList.contains('icon--menu')) {
+      burger.classList.remove('icon--menu');
+      burger.classList.add('icon--menu-click');
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const close = document.getElementById('close');
+  const burger = document.getElementById('burger');
+
+  close.addEventListener('click', () => {
+    if (burger.classList.contains('icon--menu-click')) {
+      burger.classList.remove('icon--menu-click');
+      burger.classList.add('icon--menu');
+    }
+  });
+});
